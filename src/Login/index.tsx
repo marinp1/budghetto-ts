b67e05ca22442e636c82aead1856e084a7a9d9a0
Patrik Marin <me@patrikmarin.fi>
2017-12-01 15:55:43 +0100

    Add start screen

--- src/Login/index.tsx
+++ src/Login/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import glamorous from 'glamorous';
+import Button from '../components/Button';
+import { mediaQueries } from '../styles';
 
 const Container = glamorous.div({
   background: `url("./bg.jpg")`,
@@ -9,11 +11,50 @@ const Container = glamorous.div({
   left: 0,
   right: 0,
   position: 'absolute',
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  padding: '32px',
+  alignItems: 'flex-start',
+  [mediaQueries.small]: {
+    alignItems: 'center',
+  }
+})
+
+const Title = glamorous.h1({
+  fontSize: '40px',
+  marginBottom: '40px',
+  [mediaQueries.small]: {
+    fontSize: '60px',
+  }
+})
+
+const ButtonsContainer = glamorous.div({
+  marginTop: '10px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  [mediaQueries.small]: {
+    flexDirection: 'row',
+  }
+})
+
+const ButtonContainer = glamorous.div({
+  margin: '6px 0',
+  [mediaQueries.small]: {
+    margin: '16px',
+  }
 })
 
 const Component = () => (
   <Container>
-    <h1>Hello</h1>
+    <Title>BUDGHETTO</Title>
+    <p>Start by loading a budget file or creating a new one:</p>
+    <ButtonsContainer>
+      <ButtonContainer><Button label="Open file"/></ButtonContainer>
+      <ButtonContainer><Button label="Create new"/></ButtonContainer>
+    </ButtonsContainer>
   </Container>
 )
 
---
+++ src/components/Button.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import glamorous from 'glamorous';
+
+const Container = glamorous.button({
+  backgroundColor: '#FFF',
+  height: '42px',
+  width: '120px',
+  borderRadius: '8px',
+  fontSize: '14px',
+  ':hover': {
+    backgroundColor: '#F4F4F4',
+  },
+  ':active': {
+    backgroundColor: '#F2F2F2',
+  }
+})
+
+interface ButtonProps { label: string; }
+
+const Button = (props: ButtonProps) => (
+  <Container>{props.label}</Container>
+);
+
+export default Button;
