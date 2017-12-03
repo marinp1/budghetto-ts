a99d0c9c6787bf8b17624b9270a5d827a944b24c
Patrik Marin <me@patrikmarin.fi>
2017-12-03 17:18:15 +0100

    Add prettier plugin

--- src/Navbar/index.tsx
+++ src/Navbar/index.tsx
@@ -1,50 +1,60 @@
-import * as React from 'react';
-import glamorous from 'glamorous';
-import paths from '../paths';
+import * as React from "react";
+import glamorous from "glamorous";
+import paths from "../paths";
 
 const NavbarElement = glamorous.nav({
-  background: '#000',
+  background: "#000"
 });
 
 const NavbarBrandTitle = glamorous.h6({
-  color: '#FFF',
-  marginLeft: '10px',
-  padding: '0.1rem 0',
-  ':hover': {
-    color: '#AAA',
+  color: "#FFF",
+  marginLeft: "10px",
+  padding: "0.1rem 0",
+  ":hover": {
+    color: "#AAA"
   },
-  ':active': {
-    color: '#333',
-  },
+  ":active": {
+    color: "#333"
+  }
 });
 
 const NavbarLinkElement = glamorous.a({
-  cursor: 'pointer',
-  color: '#FFF',
-  ':hover': {
-    background: 'transparent !important',
-    color: '#AAA !important',
+  cursor: "pointer",
+  color: "#FFF",
+  ":hover": {
+    background: "transparent !important",
+    color: "#AAA !important"
   },
-  ':active': {
-    color: '#333 !important',
-  },
+  ":active": {
+    color: "#333 !important"
+  }
 });
 
 const Navbar = () => (
-  <NavbarElement className="navbar" role="navigation" aria-label="main navigation">
+  <NavbarElement
+    className="navbar"
+    role="navigation"
+    aria-label="main navigation"
+  >
     <div className="navbar-brand">
       <NavbarLinkElement className="navbar-item" href={paths.home}>
-        <div style={{ display: 'flex' }}>
-          <img src="./logo_round.png"
-              alt="Budghetto"
-              width="auto" height="100%"/>
+        <div style={{ display: "flex" }}>
+          <img
+            src="./logo_round.png"
+            alt="Budghetto"
+            width="auto"
+            height="100%"
+          />
           <NavbarBrandTitle>BUDGHETTO</NavbarBrandTitle>
         </div>
       </NavbarLinkElement>
-      <button className="button navbar-burger is-black" data-target="navbarContent">
-        <span></span>
-        <span></span>
-        <span></span>
+      <button
+        className="button navbar-burger is-black"
+        data-target="navbarContent"
+      >
+        <span />
+        <span />
+        <span />
       </button>
       <div id="navbarContent" className="navbar-menu">
         <div className="navbar-start">
