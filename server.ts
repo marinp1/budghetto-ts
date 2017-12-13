893426cd1d8ac6878b096c8ec23e8c2b85ec2b80
Patrik Marin <me@patrikmarin.fi>
2017-12-13 20:27:38 +0100

    Add sample endpoint

--- server.ts
+++ server.ts
@@ -13,15 +13,21 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // QUERIES
-app.get('/api/accounts', (req, res) => {
-  const param = req.query.q;
-  console.log(param);
+app.get('/api/:userId/accounts',(
+  req: express.Request,
+  res: express.Response) => {
+  const userId = req.params.userId;
+  res.send('user ' + userId);
 });
 
-app.get('*', (req, res) => {
-  console.log('Not found, send error');
-  // TODO;
+app.get('*', (req: express.Request,
+              res: express.Response) => {
+  send404Response(res);
 });
+
+function send404Response(res: express.Response) {
+  res.send('404 - not found');
+}
 
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`);
